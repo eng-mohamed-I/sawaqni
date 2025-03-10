ac4673dd225b073bb3215a7642fc84cd1c9c8269
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2025-03-10 20:37:03 +0200

    Feature: add cart schema

---
+++ src/core/schemas/cart.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { User } from './user.schema';
+import { Product } from './product.schema';
+//=========================================================
+@Schema({ timestamps: true, versionKey: false })
+export class Cart {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  user: User;
+
+  @Prop({
+    type: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    required: true,
+  })
+  products: [{ product: Product; quentity: number }];
+}
+//=========================================================
+const CartSchema = SchemaFactory.createForClass(Cart);
